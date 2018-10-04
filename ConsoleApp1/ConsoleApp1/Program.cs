a3bc526eb1dfc31d264ea7b9133668e7dd074b67
liquidsamlol <liquidsamlol@gmail.com>
2018-10-05 01:34:51 +0300

    done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the operation");
+            char oper = Convert.ToChar(Console.ReadLine());
+
+            int result;
+
+            if (oper=='+')
+            {
+                result = num1 + num2;
+                Console.WriteLine("Result = " + result);
+
+            }
+            else if (oper=='-')
+            {
+                result = num1 - num2;
+                Console.WriteLine("Result = " + result);
+            }
+            else if (oper=='*')
+            {
+                result = num1 * num2;
+                Console.WriteLine("Result = " + result);
+            }
+            else if (oper=='/')
+            {
+                result = num1 / num2;
+                Console.WriteLine("Result = " + result);
+            }
+            else
+            {
+                Console.WriteLine("Wrong operation character, please try again");
+            }
+        }
+    }
+}
